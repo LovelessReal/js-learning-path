1ed2a5c5ac1e2dfd8c5571f80331c4c337ba247f
Valeria Andreevna <lovelessreal@gmail.com>
2025-03-12 12:00:00 +0300

    test

--- lesson 5/script.js
+++ lesson 5/script.js
@@ -3,9 +3,28 @@ const year = 2025
 if (year == 2025) {
     console.log('Ура!')
     console.log('Самое время изучать JS')
-} else if (year < 2025){
+} else if (year < 2025){ //иначе если
  console.log('О, да ты путешественник во времени')
 } else {
     console.log('Too late...')
 }
 
+const x = 9
+let message = null //обозначаем так потому что собираемся перезаписать переменную
+
+if (x == 20) {
+    message = 'Say something'
+}
+else if (x > 20) {
+    message = 'High way to hell'
+}
+else {
+    message = 'i like to move it'
+}
+
+console.log(message)
+
+//Есть краткая запись с 2мя вариантами развития
+
+const y = 15
+const words = y = 15 ? 'Lets do is' : ''
