149c3110380397ec3ea6f9be83203d83df94f62f
Valeria Andreevna <lovelessreal@gmail.com>
2025-03-14 12:00:00 +0300

    new

---
+++ lesson 5/script.js
@@ -0,0 +1,9 @@
+const year = 2025
+if (year == 2025) {
+    console.log('Ура!')
+    console.log('Самое время изучать JS')
+} else if (year < 2025){
+ console.log('О, да ты путешественник во времени')
+} else {
+    console.log('Too late...')
+}
