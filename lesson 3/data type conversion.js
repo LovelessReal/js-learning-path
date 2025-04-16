c8fa12f1bfa560c403a7d92d2b6860c3f425413f
Valeria Andreevna <lovelessreal@gmail.com>
2025-04-16 12:00:00 +0300

    new

--- lesson 3/data type conversion.js
+++ lesson 3/data type conversion.js
@@ -3,3 +3,4 @@ const numb = 111
 const str = '222'
 const result = numb + str
 console.log (result) //В консоли будет выведено 111222. Числа склеются 
+console.log (typeof result) //result отразится как данные со строчным типом
