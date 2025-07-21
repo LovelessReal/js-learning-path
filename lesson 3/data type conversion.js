eaf0b4adc6d8156e279b05638bdb10e47ecbae3d
Valeria Andreevna <lovelessreal@gmail.com>
2025-07-21 11:48:34 +0300

    new

--- lesson 3/data type conversion.js
+++ lesson 3/data type conversion.js
@@ -42,3 +42,13 @@ console.log (
 console.log (
     Number (false) // false преобразуется в 0
 )
+
+ console.log (Boolean(-1)) //Преобразуется в true
+ console.log (Boolean(0)) //Преобразуется в false
+ console.log (Boolean(1)) //Преобразуется в true
+ console.log (Boolean(2)) //Преобразуется в true
+
+  console.log (Boolean('текст')) //Преобразуется в true
+ console.log (Boolean('0')) //Преобразуется в false
+ console.log (Boolean(' ')) //Преобразуется в true
+ console.log (Boolean('')) //Отсутствие всяких симолов (даже пробела) преобразует строку в false
