ba2b93826e6afdbb4fdc55ebaf80353196c30cb8
Valeria Andreevna <lovelessreal@gmail.com>
2025-08-03 21:47:49 +0300

    new lines in lesson-8

---
+++ lesson 8/script.js
@@ -0,0 +1,10 @@
+//Функции в JS
+
+console.log ('Привет из консоли')
+ 
+const number_from_string = Number('100')
+const string_from_number = String(100)
+const boolean_fron_null = Boolean(null)
+
+alert('Привет из alert')
+confirm('Подтверждение действий')
