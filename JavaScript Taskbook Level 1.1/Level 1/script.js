b6c3f820e0579de521a78b6135e9a3a9ed805f21
Valeria Andreevna <lovelessreal@gmail.com>
2025-08-26 15:24:39 +0300

    Tasks 3 and 4
    Add new tasks

--- JavaScript Taskbook Level 1.1/Level 1/script.js
+++ JavaScript Taskbook Level 1.1/Level 1/script.js
@@ -16,3 +16,21 @@ else {
 let user_string = prompt ('Введите любой текст')
 let user_string_length = user_string.length
 console.log (`Длина введенной пользователем строки ${user_string_length}`)
+
+//Задача 3. Дана строка. Выведите в консоль последний символ строки.
+
+let user_sentence = prompt ('Введите сюда что-нибудь')
+let last_letter = user_sentence.slice(-1)
+console.log(last_letter)
+
+//Дано число. Проверьте, четное оно или нет.
+
+let user_number = prompt ('Введите какое-то число')
+user_number = Number(user_number)
+ k = (user_number % 2)
+ if (k > 0) {
+    console.log('Было введено нечетное число')
+ }
+ if (k == 0) {
+    console.log ('Было введено четное число')
+ }
