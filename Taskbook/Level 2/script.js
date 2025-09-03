b7f80e3e212d179cf18f3f95931ac0b9d17c4edc
Valeria Andreevna <lovelessreal@gmail.com>
2025-09-03 13:27:35 +0300

    new files

---
+++ Taskbook/Level 2/script.js
@@ -0,0 +1,7 @@
+//Даны два числа. Проверьте, что первые цифры этих чисел совпадают.
+
+let first_number = prompt ('Введите первое число')
+let second_number = prompt ('Введите второе число')
+if (first_number.charAt(0) = second_number.charAt(0)) {
+    console.log ('Первые цифры этих чисел совпадают')
+}
